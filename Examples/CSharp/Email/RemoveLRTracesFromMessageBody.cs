6e210bcb23deb233f229e90955852739ddae114f
kashifiqb <kashif.iqbal@aspose.com>
2017-01-19 22:23:31 +0500

    Added Code Samples for Aspose.Email for .NET 17.1.0
    Changes are related to the new features and enhancements included in
    this release.

---
+++ Examples/CSharp/Email/RemoveLRTracesFromMessageBody.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Email
+{
+    class RemoveLRTracesFromMessageBody
+    {
+        public static void Run()
+        {
+            // ExStart:CreateNewMailMessage
+            string dataDir = RunExamples.GetDataDir_Email();
+
+            //sample input file
+            string fileName = "EmlWithLinkedResources.eml";
+
+            //Load the test message with Linked Resources
+            MailMessage msg = MailMessage.Load(dataDir + fileName);
+
+            //Remove a LinkedResource
+            msg.LinkedResources.RemoveAt(0, true);
+
+            //Now clear the Alternate View for linked Resources
+            msg.AlternateViews[0].LinkedResources.Clear(true);
+            // ExEnd:CreateNewMailMessage
+        }
+    }
+}
