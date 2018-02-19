75d63c8835686a0f17f17ccf8229266b0f423ae4
Kashif Iqbal <kashifiqb@gmail.com>
2018-02-19 19:22:31 +0500

    Replaced Nuget and Forum links within each code sample to the latest ones.

--- Examples/CSharp/Outlook/AddMessagesToPSTFiles.cs
+++ Examples/CSharp/Outlook/AddMessagesToPSTFiles.cs
@@ -5,9 +5,9 @@ using System.IO;
 /* This project uses Automatic Package Restore feature of NuGet to resolve Aspose.Email for .NET 
    API reference when the project is build. Please check https://Docs.nuget.org/consume/nuget-faq 
    for more information. If you do not wish to use NuGet, you can manually download 
-   Aspose.Email for .NET API from http://www.aspose.com/downloads, 
+   Aspose.Email for .NET API from https://www.nuget.org/packages/Aspose.Email/, 
    install it and then add its reference to this project. For any issues, questions or suggestions 
-   please feel free to contact us using http://www.aspose.com/community/forums/default.aspx            
+   please feel free to contact us using https://forum.aspose.com/c/email            
 */
 
 namespace Aspose.Email.Examples.CSharp.Email.Outlook
