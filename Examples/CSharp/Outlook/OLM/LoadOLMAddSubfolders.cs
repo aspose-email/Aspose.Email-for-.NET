20e3510381794a377bdf94d10c602611aa66e083
aspose-email <muzammil.khan@aspose.com>
2018-10-30 10:22:37 +0500

    Updates for Aspose.Email for .NET 18.10 Release

---
+++ Examples/CSharp/Outlook/OLM/LoadOLMAddSubfolders.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Aspose.Email.Storage.Olm;
+using Aspose.Email.Mapi;
+
+namespace Aspose.Email.Examples.CSharp.Email.Outlook.OLM
+{
+    class LoadOLMAddSubfolders
+    {
+        public static void Run()
+        {
+            // The path to the File directory.
+            string dataDir = RunExamples.GetDataDir_Outlook();
+            string dst = dataDir + "OutlookforMac.olm";
+
+            using (OlmStorage storage = new OlmStorage(dst))
+            {
+                foreach (OlmFolder folder in storage.FolderHierarchy)
+                {
+                    if (folder.HasMessages)
+                    {
+                        // extract messages from folder
+                        foreach (MapiMessage msg in storage.EnumerateMessages(folder))
+                        {
+                            Console.WriteLine("Subject: " + msg.Subject);
+                        }
+                    }
+
+                    // read sub-folders
+                    if (folder.SubFolders.Count > 0)
+                    {
+                        foreach (OlmFolder sub_folder in folder.SubFolders)
+                        {
+                            Console.WriteLine("Subfolder: " + sub_folder.Name);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
